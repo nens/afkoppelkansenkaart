9a2e19ae1cf5ca7ef11842ef7641133fdd426899
leendertvanwolfswinkel <leendert.vanwolfswinkel@nelen-schuurmans.nl>
2022-01-26 14:12:17 +0100

    iets

--- afkoppelkansenkaart/afkoppelkansenkaart_dockwidget.py
+++ afkoppelkansenkaart/afkoppelkansenkaart_dockwidget.py
@@ -27,8 +27,10 @@ import sys
 sys.path.append(os.path.dirname(__file__))
 
 from qgis.PyQt import QtGui, QtWidgets, uic
-from qgis.PyQt.QtCore import pyqtSignal
-from qgis.core import QgsProject,QgsVectorLayer,QgsRasterLayer,QgsMapLayerProxyModel
+from qgis.PyQt.QtCore import pyqtSignal, QSettings
+from qgis.core import QgsProject,QgsVectorLayer,QgsRasterLayer,QgsMapLayerProxyModel, QgsProcessingFeatureSourceDefinition
+
+import processing
 
 import psycopg2
 
@@ -45,18 +47,35 @@ class AfkoppelKansenKaartDockWidget(QtWidgets.QDockWidget,FORM_CLASS):
     def __init__(self,parent=None):
         """Constructor."""
         super(AfkoppelKansenKaartDockWidget,self).__init__(parent)
+
+        self.parcel_layer_id = None
+
         self.setupUi(self)
         self.pushButton_PercelenWFS.clicked.connect(self.add_parcel_wfs)
         self.pushButton_CheckConnection.clicked.connect(self.update_postgis_connection_status)
+        self.populate_combobox_postgis_databases()
 
     def closeEvent(self,event):
         self.closingPlugin.emit()
         event.accept()
 
-    @staticmethod
+    def list_postgis_connections(self):
+        s = QSettings()
+        s.beginGroup("PostgreSQL/connections")
+        result = s.childGroups()
+        s.endGroup()
+        return result
+
+    def populate_combobox_postgis_databases(self):
+        self.comboBox_PostGISDatabases.clear()
+        self.comboBox_PostGISDatabases.addItems(self.list_postgis_connections())
+
     def add_parcel_wfs(self):
+        if self.parcel_layer_id:
+            QgsProject.instance().removeMapLayer(self.parcel_layer_id)
         vlayer = QgsVectorLayer(PARCELS_WFS_URL, "Kadastraal perceel", "WFS")
         QgsProject.instance().addMapLayer(vlayer)
+        self.parcel_layer_id = vlayer.id()
 
     def get_postgis_connection_params(self):
         host = self.lineEdit_Host.text()
@@ -74,12 +93,34 @@ class AfkoppelKansenKaartDockWidget(QtWidgets.QDockWidget,FORM_CLASS):
         }
         return db_params
 
-    def update_postgis_connection_status(self):
+    def postgis_connection_is_valid(self):
         try:
             conn = psycopg2.connect(**self.get_postgis_connection_params())
         except psycopg2.OperationalError:
-            self.label_StatusValue.setText('Invalid connection details')
-            return
-        self.label_StatusValue.setText('Valid connection details')
+            return False
         conn.close()
-        return
+        return True
+
+    def update_postgis_connection_status(self):
+        if self.postgis_connection_is_valid():
+            self.label_StatusValue.setText('Valid connection details')
+        else:
+            self.label_StatusValue.setText('Invalid connection details')
+
+    def import_parcels_wfs_to_postgis(self):
+        feature_source = QgsProcessingFeatureSourceDefinition(source=self.parcel_layer_id, selectedFeaturesOnly = True)
+        processing_args = {
+            'INPUT': feature_source,
+            'DATABASE': 'localhost - sandbox',
+            'SCHEMA': 'public',
+            'TABLENAME': 'kadastraal_perceel',
+            'PRIMARY_KEY': 'id',
+            'GEOMETRY_COLUMN': 'geom',
+            'ENCODING': 'UTF-8',
+            'OVERWRITE': True,
+            'CREATEINDEX': True,
+            'LOWERCASE_NAMES': True,
+            'DROP_STRING_LENGTH': True,
+            'FORCE_SINGLEPART': False
+        }
+        processing.run("qgis:importintopostgis",
