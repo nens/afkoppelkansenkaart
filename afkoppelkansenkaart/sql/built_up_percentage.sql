31a8e1d9a2461ba197aade772afcad4f470f5050
leendertvanwolfswinkel <leendert.vanwolfswinkel@nelen-schuurmans.nl>
2022-03-30 17:13:48 +0200

    Update built_up_percentage.sql

--- afkoppelkansenkaart/sql/built_up_percentage.sql
+++ afkoppelkansenkaart/sql/built_up_percentage.sql
@@ -3,15 +3,14 @@ ALTER TABLE kadastraal_perceel_subdivided ADD COLUMN IF NOT EXISTS percentage_be
 
 WITH intersection AS (
 	SELECT 	perc.id, 
-			COALESCE(ST_Area(ST_Intersection(perc.geom, pand.geom)), 0) AS oppervlakte_bebouwing
-	FROM	bgt_inlooptabel AS pand
-	LEFT JOIN 	kadastraal_perceel_subdivided AS perc
-	ON 		ST_Intersects(pand.geom, perc.geom)
-	WHERE 	pand.type_verharding = 'dak'
+			ST_Area(ST_Intersection(perc.geom, pand.geom)) AS oppervlakte_bebouwing
+	FROM	kadastraal_perceel_subdivided AS perc
+	LEFT JOIN 	bgt_inlooptabel AS pand
+	ON 		ST_Intersects(pand.geom, perc.geom) AND pand.type_verharding = 'dak'
 )
 UPDATE 	kadastraal_perceel_subdivided AS tgt
-SET 	oppervlakte_bebouwing = src.oppervlakte_bebouwing,
-		percentage_bebouwing = src.oppervlakte_bebouwing / ST_Area(tgt.geom)
+SET 	oppervlakte_bebouwing = COALESCE(src.oppervlakte_bebouwing, 0),
+		percentage_bebouwing = COALESCE(src.oppervlakte_bebouwing / ST_Area(tgt.geom) * 100, 0)
 FROM 	intersection AS src
 WHERE 	src.id = tgt.id
 ;
