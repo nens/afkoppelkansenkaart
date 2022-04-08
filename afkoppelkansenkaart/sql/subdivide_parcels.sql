b52691632b2111e952f00cc524906d08933027ae
leendertvanwolfswinkel <leendert.vanwolfswinkel@nelen-schuurmans.nl>
2022-04-08 10:31:14 +0200

    put creation of view in separate sql file

--- afkoppelkansenkaart/sql/subdivide_parcels.sql
+++ afkoppelkansenkaart/sql/subdivide_parcels.sql
@@ -205,33 +205,3 @@ DROP TABLE IF EXISTS rechte_percelen_selectie;
 DROP TABLE IF EXISTS geknipte_percelen_recht;
 DROP TABLE IF EXISTS percelen_clean_teruggedraaid;
 DROP TABLE IF EXISTS kadastraal_perceel;
-
-
--- Create view for exporting to Geopackage
-DROP VIEW IF EXISTS perceel;
-CREATE OR REPLACE VIEW perceel AS 
-SELECT	id, 
-		brk_lokaalid, 
-		brk_perceelnummer, 
-		oppervlakte_perceel, 
-		gemeentelijk_eigendom, 
-		oppervlakte_bebouwing, 
-		percentage_bebouwing, 
-		verhard_oppervlak, 
-		verhard_percentage,
-		maaiveldhoogte, 
-		bodemsoort, 
-		doorlatendheid_bodem, 
-		ghg_tov_maaiveld, 
-		afstand_tot_bergingslocatie, 
-		code_dichtsbijzijnde_rioolleiding, 
-		type_rioolstelsel, 
-		kwetsbaarheid_oppervlaktewater, 
-		aantal_keer_verpompen, 
-		afstand_tot_rwzi, 
-		type_gebied,
-		geom
-FROM 	kadastraal_perceel_subdivided
-;
-
-
